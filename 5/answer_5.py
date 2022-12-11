6d173d10048bd4f9c0bc9a1f440604044cb0eaa1
Tom Baxendale <tom@klikkie.com>
2022-12-11 21:21:33 +0100

    question 5a & b

--- 5/answer_5.py
+++ 5/answer_5.py
@@ -11,10 +11,11 @@ crates_final = []
 commands = input.split("\n\n")[1]
 commands = commands.split("\n")
 
-# create new dictionary with list as default value
-# new keys' values can be instantly appended as a list
+# create new dictionary with list as default value for new keys. enables instant list appending to new keys
 crate_dict = defaultdict(list)
 
+
+# process the text input: remove every fourth character in each row, leaving behind only crates or crate spaces
 def remove_nth_char(string, n):
 
     string = list(string)
@@ -27,7 +28,7 @@ for row in crates_raw:
         break
     else:
         row = remove_nth_char(row, 4)
-        row = row.replace("   ","[ ]") # log empty crates
+        row = row.replace("   ","[ ]") # log crate spaces
         row = row.replace("[","").replace("]","") # remove square brackets
         crates_final.append(list(row))
 
@@ -35,10 +36,10 @@ for row in crates_final: # iterate through each row of crates and add to the cor
     i = 1
     for crate in row:
         if crate != " ":
-            crate_dict[i].append(crate) # instantly append because of list defaultdict
+            crate_dict[i].append(crate) # list is created upon key creation (defaultdict)
         i += 1
 
-# sort dict by key values
+# sort dict by key values (1, 2, 3...)
 crate_dict = dict(sorted(crate_dict.items()))
 
 def move_crate(command):
@@ -46,16 +47,35 @@ def move_crate(command):
     _to = int(command.split(" ")[5])
     
     crate = crate_dict[_from][0] # first crate in _from crate stack
-    crate_dict[_to].insert(0, crate) # add to beginning (top) of _to crate stack
     del crate_dict[_from][0] # delete from _from crate stack
+    crate_dict[_to].insert(0, crate) # add to beginning (top) of _to crate stack
 
 def move_crates(command):
     _move = int(command.split(" ")[1])
     for i in range(0, _move):
         move_crate(command)
 
-for command in commands:
-    move_crates(command = command)
+#for command in commands:
+#    move_crates(command = command)
 
-answer_5a = [value[0] for key, value in crate_dict.items()]
-print(answer_5a)
+#answer_5a = [value[0] for key, value in crate_dict.items()]
+#print(answer_5a)
+
+# create a block of crates, remove from original stack and add to new stack in reverse order
+def move_crate_block(command):
+    _move = int(command.split(" ")[1])
+    _from = int(command.split(" ")[3])
+    _to = int(command.split(" ")[5])
+    
+    crate_block = crate_dict[_from][0:_move]
+    del crate_dict[_from][0:_move]
+    crate_block.reverse() # first n crates within _from crate stack and reverse crate order (bottom crate now moves first)
+
+    for crate in crate_block:
+        crate_dict[_to].insert(0, crate)
+
+for command in commands:
+   move_crate_block(command = command)
+
+answer_5b = [value[0] for key, value in crate_dict.items()]
+print(answer_5b)
